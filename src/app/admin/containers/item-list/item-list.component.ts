8c4abbd4e543c28f70f84d1dc5b61796f91ebec0
Nhan Nguyen <Nhan.Nguyen@truecommerce.com>
2023-04-10 17:04:00 +0700

    Create an injectable service and share state in components via services

--- src/app/admin/containers/item-list/item-list.component.ts
+++ src/app/admin/containers/item-list/item-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Item } from '../../models/item.model';
+import { ItemService } from '../../services/item.service';
 
 @Component({
   selector: 'app-item-list',
@@ -11,32 +12,11 @@ export class ItemListComponent implements OnInit {
   items!: Item[];
   item!: Item;
 
+  constructor(private itemService: ItemService) {
+
+  }
   ngOnInit(): void {
-    this.items = [
-      {
-        id: '1',
-        name: 'Just Chocolate',
-        icon: 'just-chocolate',
-        promo: 'new',
-        price: 100,
-        description: 'For the pure chocoholic'
-      },
-      {
-        id: '2',
-        name: 'Glazed Fudge',
-        icon: 'glazed-fudge',
-        promo: 'limited',
-        price: 229,
-        description: 'Sticky perfection.',
-      },
-      {
-        id: '3',
-        name: 'Caramel Swirl',
-        icon: 'caramel-swirl',
-        price: 129,
-        description: 'Chocolate drizzled with caramel.',
-      },
-    ];
+    this.items = this.itemService.items;
     this.item = this.items[0];
   }
   trackById(index: number, item: Item) {
