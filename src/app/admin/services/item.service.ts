32ce823b5ec7634ea4e40ac5d91e206e1747e22e
Nhan Nguyen <Nhan.Nguyen@truecommerce.com>
2023-04-10 23:28:12 +0700

    use retryWhen to delay retries

--- src/app/admin/services/item.service.ts
+++ src/app/admin/services/item.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Item } from '../models/item.model';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { catchError, map, of, retry, tap, throwError } from 'rxjs';
+import { catchError, delay, map, of, retry, retryWhen, take, tap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +14,8 @@ export class ItemService {
     if (this.items.length) {
       return of(this.items);
     }
-    return this.httpClient.get<Item[]>('api/items').pipe(tap(items => this.items = items), retry(2));
+    return this.httpClient.get<Item[]>('api/items').pipe(tap(items => this.items = items),
+      retryWhen((errors) => errors.pipe(tap(console), delay(2000), take(2))), catchError(this.handleError));
   }
 
   readOne(id: string) {
